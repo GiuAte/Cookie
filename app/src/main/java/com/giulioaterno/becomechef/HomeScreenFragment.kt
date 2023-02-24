0756f22497a1f666355bd83a715c208595958e09
GiuAte <giulioaterno@gmail.com>
2023-02-24 17:12:08 +0100

    Created onClickItem for HomePageRV to Profile Fragment

--- app/src/main/java/com/giulioaterno/becomechef/HomeScreenFragment.kt
+++ app/src/main/java/com/giulioaterno/becomechef/HomeScreenFragment.kt
@@ -5,15 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.giulioaterno.becomechef.databinding.HomescreenPageBinding
+import com.google.gson.Gson
 
-@Suppress("UNREACHABLE_CODE")
+
 class HomeScreenFragment: Fragment() {
-
-    val searchView = binding.searchBar
-    val searchIcon = binding.searchBarLogo
 
     private var bindingHomePage: HomescreenPageBinding? = null
     private val binding get() = bindingHomePage!!
@@ -26,13 +25,6 @@ class HomeScreenFragment: Fragment() {
         bindingHomePage = HomescreenPageBinding.inflate(inflater, container, false)
         setRoundedIconRecyclerView(DataObject.usernameList)
         return binding.root
-        searchIcon.setOnClickListener {
-            if (searchView.visibility == View.GONE) {
-                searchView.visibility = View.VISIBLE
-            } else {
-                searchView.visibility = View.GONE
-            }
-        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -43,7 +35,12 @@ class HomeScreenFragment: Fragment() {
     }
 
     private fun setRoundedIconRecyclerView(list: List<ProfileRoundedIcon>) {
-        val adapterProfileRoundedIcon = ProfileRoundedIconAdapter(list)
+        val bundle = Bundle()
+        val adapterProfileRoundedIcon = ProfileRoundedIconAdapter(list) {
+            val profile = Gson().toJson(it)
+            bundle.putString("profile", profile)
+            Navigation.findNavController(requireView()).navigate(R.id.action_homeScreenFragment_to_profilePageFragment, bundle)
+        }
         binding.bestChefRV.apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
             adapter = adapterProfileRoundedIcon
--- app/src/main/java/com/giulioaterno/becomechef/ProfileRoundedIconAdapter.kt
+++ app/src/main/java/com/giulioaterno/becomechef/ProfileRoundedIconAdapter.kt
@@ -5,13 +5,20 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.giulioaterno.becomechef.databinding.BestChefRecyclerViewItemBinding
 
-class ProfileRoundedIconAdapter(val usernames: List<ProfileRoundedIcon>) :
+class ProfileRoundedIconAdapter(
+    val usernames: List<ProfileRoundedIcon>,
+    private val onItemClicked: (ProfileRoundedIcon) -> Unit
+) :
     RecyclerView.Adapter<ProfileRoundedIconAdapter.ProfileViewHolder>() {
     class ProfileViewHolder(var binding: BestChefRecyclerViewItemBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
-        val view = BestChefRecyclerViewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view = BestChefRecyclerViewItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
         return ProfileViewHolder(view)
     }
 
@@ -20,10 +27,15 @@ class ProfileRoundedIconAdapter(val usernames: List<ProfileRoundedIcon>) :
     }
 
     override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
-        with(holder){
-            with(usernames[position]){
+        with(holder) {
+            with(usernames[position]) {
                 holder.binding.myTextView.text = this.name
+                holder.binding.profileImage.setImageResource(this.image)
+                holder.binding.profileImage.setOnClickListener {
+                    onItemClicked(this)
+                }
             }
         }
     }
+
 }
